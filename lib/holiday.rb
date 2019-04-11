614e5d4261d692a781bcca17f84cb43a7150069b
Daniel Bovington <d.bovington@gmail.com>
2019-04-11 19:34:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -81,7 +81,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 bbq_holiday = []
-holiday_hash 
+holiday_supplies
 end
 
 
